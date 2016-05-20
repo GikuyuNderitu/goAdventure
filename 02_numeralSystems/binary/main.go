9e20c903cf6f52fb375176428059b5fa4fe5f285
GikuyuNderitu <gikuyunderitu@gmail.com>
2016-05-20 11:19:05 -0400

    Delete prev directories

--- 02_numeralSystems/binary/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-
-    fmt.Printf("%d becomes %b\n", 42, 42)
-}
--- 02_numeralSystems/hexidecimal/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Printf("%d becomes %x\n", 42, 42)
-}
